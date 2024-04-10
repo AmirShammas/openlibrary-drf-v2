9eaa32562d3c4573abf787ddabe2b6fce9418a44
ShammasAmir <amirshammas@gmail.com>
2024-04-10 11:05:12 +0330

    Create model for Book and Author.Run migrations.

--- scraper_project/scraper/admin.py
+++ scraper_project/scraper/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.admin import register
+from .models import Author, Book
 
-# Register your models here.
+
+@register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "url", "is_active",
+                    "created_at", "updated_at")
+    list_display_links = ("id", "name")
+    list_filter = ("is_active", "created_at", "updated_at")
+    list_editable = ("is_active",)
+    search_fields = ("name",)
+
+
+@register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "url", "cover", "author",
+                    "is_active", "created_at", "updated_at")
+    list_display_links = ("id", "title")
+    list_filter = ("is_active", "created_at", "updated_at", "author",)
+    list_editable = ("is_active",)
+    search_fields = ("title", "is_active", "author__name",)
--- scraper_project/scraper/models.py
+++ scraper_project/scraper/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+
+class MyBaseModel(models.Model):
+    is_active = models.BooleanField(
+        default=False,
+        verbose_name="Is Active",
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Created At",
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name="Updated At",
+    )
+
+    class Meta:
+        abstract = True
+        ordering = ("pk",)
+
+    def __str__(self):
+        raise NotImplementedError("Implement __str__ method")
+
+
+class Author(MyBaseModel):
+    name = models.CharField(max_length=1000, null=True,
+                            blank=True, verbose_name="Name")
+    url = models.URLField(null=True, blank=True, verbose_name="URL")
+
+    class Meta:
+        verbose_name = "Author"
+        verbose_name_plural = "Authors"
+        ordering = ("id",)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Book(MyBaseModel):
+    title = models.CharField(max_length=1000, null=True,
+                             blank=True, verbose_name="Title")
+    url = models.URLField(null=True, blank=True, verbose_name="URL")
+    cover = models.URLField(null=True, blank=True, verbose_name="Cover")
+    author = models.ForeignKey(Author, null=True, blank=True,
+                               on_delete=models.PROTECT, related_name="books", verbose_name="Author")
+
+    class Meta:
+        verbose_name = "Book"
+        verbose_name_plural = "Books"
+        ordering = ("id",)
+
+    def __str__(self) -> str:
+        return self.title
